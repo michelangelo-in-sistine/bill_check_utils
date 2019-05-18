916610eba432b4d7d614503e13470edc46581db7
Mac <machael@gmail.com>
2019-05-18 22:13:38 +0800

    1. unsorted作为最后一项 2. 按detail排序 3. 计算分类总和

--- bill_checker.py
+++ bill_checker.py
@@ -10,14 +10,16 @@ cmb_pattern = r"(\d+) \d+ (.+) [￥$] ([\d,]+\.\d+) 7007 (.+) ([\d,]+\.\d+)"
 
 sort_rules = {"food": ["肯德基", "老盛昌", "全家", "水果", "汉堡王", "星巴克", "必胜客", "红宝石", "多乐之日", 
                       "金拱门", "鸡排", "鲜芋仙", "四海游龙", "雪芙", "汤包", "泡芙", "餐饮", "呷哺呷哺", "玛格萝妮", "烤肉",
-                      "翠华", "美团 && amount >= 20"],
-            "life": ["绿地优鲜超市", "华住", "宝岛眼镜", "上蔬永辉"],
+                      "翠华", "美团 && amount >= 20", "集贸市场", "莉莲", "煌上煌", "Mo-Mo牧场", "阿文大虾"],
+            "life": ["绿地优鲜超市", "华住", "宝岛眼镜", "上蔬永辉", "优衣库", "迪亚天天", "窝的鲜花", "茶阁里的猫"
+                     "联华超市"],
             "traffic": ["嘀嗒", "嘀嘀", "美团 && amount < 20"],
             "shopping": ["京东", "久光", ],
             "basic": ["城投水务", "电力公司"],
             "entertain": ["幸福蓝海", "格瓦拉", "主题乐园", ],
             "health": ["复旦大学附属华山医院"],
-            "raise child": ["儿童医院", "第十人民医院", "网易考拉", "卡通尼", "麦淘亲子", "亲子", "教育"],
+            "raise child": ["儿童医院", "第十人民医院", "网易考拉", "卡通尼", "麦淘亲子", "亲子",],
+            "education": ["otto2",  "教育"]
             }
 
 
@@ -33,13 +35,13 @@ class Record():
             assert 0
         
     def __str__(self):
-        return "{:<12.2f} {:<12s} {:<s}".format(self.amount, self.date, self.detail)
+        return "{:<8.2f} {:<8s} {:<s}".format(self.amount, self.date, self.detail)
     
     def sort(self, category=None):
         """ 获得一条记录的分类
         """
         if category is None:
-            category = list(keywords_dict.keys())
+            category = list(sort_rules.keys())
         
         for i, cat in enumerate(category):
             try:
@@ -63,39 +65,36 @@ class Record():
                 raise e
                 
         else:
-            return None
+            return -1, 'unsorted'
         
         
 def org(bill_text):
     entrys = [entry.strip() for entry in bill_text.split('\n') if len(entry) > 8]
     category = list(sort_rules.keys())
-    sorted_bill = [[cat] for cat in category]
-    remained_bill = ['remained']
+    sorted_bill = [[cat, []] for cat in category]
+    sorted_bill.append(['unsorted', []])
     
+    # 
     for each_entry in entrys:
         a = Record(each_entry)
         rslt = a.sort(category)
-        if rslt is None:
-            remained_bill.append(a)
-        else:
-            sorted_bill[rslt[0]].append(a)
+        sorted_bill[rslt[0]][1].append(a)
+        
+    # 按details排序
+    for entrys in sorted_bill:
+        entrys[1].sort(key=lambda x: x.detail)
     
+    # print
     for cate in sorted_bill:
         if len(cate) > 1:
             print('\n',cate[0])
-            for item in cate[1:]:
+            sum_t = 0
+            for item in cate[1]:
+                sum_t += item.amount
                 print(item)
+            print('--total:{:.2f}'.format(sum_t))
 
-    print('\n',remained_bill[0])
-    for item in remained_bill[1:]:
-        print(item)
-    
-    
-#    print('\n\n\n\nreversed order:')
-#    for item in entrys[::-1]:
-#        print(item)
-
-    return sorted_bill, remained_bill
+    return sorted_bill
 
 if "__main__" == __name__:
     bill = """
