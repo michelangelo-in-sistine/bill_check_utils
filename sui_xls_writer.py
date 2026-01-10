cda276187d5bca3a61cfae812d70f0e868c4bd94
lvhaifeng@X17 <lvhaifeng@home>
2026-01-11 02:01:10 +0800

    用opencode重构了xls的输出与现在的模板一致,现在导入ok

--- sui_xls_writer.py
+++ sui_xls_writer.py
@@ -12,8 +12,8 @@ import xlwt
 import os
 
 class SuiEntry():
-    def __init__(self, date='', cat1='', cat2='', account1='', account2='', 
-                 amount=0, member='', merchant='', project='', detail='', classification=0):
+    def __init__(self, date='', cat1='', cat2='', account1='', account2='',
+                 amount=0.0, member='', merchant='', project='', detail='', classification=0):
         self.date = date
         self.cat1 = cat1
         self.cat2 = cat2
@@ -37,43 +37,78 @@ class SuiXlsTemplate():
         self.file_path = file_path
         self.workbook = xlwt.Workbook(encoding='utf-8')
 
-        titles = '交易类型 日期 分类 子分类 账户1 账户2 金额 成员 商家 项目 备注'
+        # 支出工作表：10列
+        expense_titles = '交易类型 日期 分类 子分类 支出账户 金额 成员 商家 项目 备注'
         self.sheet_expense = self.workbook.add_sheet("支出")
-        add_title(self.sheet_expense, titles)
-    
+        add_title(self.sheet_expense, expense_titles)
+
+        # 收入工作表：10列
+        income_titles = '交易类型 日期 分类 子分类 收入账户 金额 成员 商家 项目 备注'
         self.sheet_income = self.workbook.add_sheet("收入")
-        add_title(self.sheet_income, titles)
-        
+        add_title(self.sheet_income, income_titles)
+
+        # 转账工作表：9列（无分类列）
+        transfer_titles = '交易类型 日期 转出账户 转入账户 金额 成员 商家 项目 备注'
         self.sheet_transfer = self.workbook.add_sheet("转账")
-        add_title(self.sheet_transfer, titles)
+        add_title(self.sheet_transfer, transfer_titles)
 
         self.sheet_entry_index_expense = 1
         self.sheet_entry_index_income = 1
+        self.sheet_entry_index_transfer = 1
 
     def add_entry(self, entry):
         if entry.classification == 0:
+            # 支出工作表：10列
             sheet = self.sheet_expense
             index = self.sheet_entry_index_expense
             transaction_type = '支出'
             self.sheet_entry_index_expense += 1
 
-        else:
+            sheet.write(index, 0, transaction_type)
+            sheet.write(index, 1, entry.date)
+            sheet.write(index, 2, entry.cat1)
+            sheet.write(index, 3, entry.cat2)
+            sheet.write(index, 4, entry.account1)  # 支出账户
+            sheet.write(index, 5, entry.amount)
+            sheet.write(index, 6, entry.member)
+            sheet.write(index, 7, entry.merchant)
+            sheet.write(index, 8, entry.project)
+            sheet.write(index, 9, entry.detail)
+
+        elif entry.classification == 1:
+            # 收入工作表：10列
             sheet = self.sheet_income
             index = self.sheet_entry_index_income
             transaction_type = '收入'
             self.sheet_entry_index_income += 1
 
-        sheet.write(index, 0, transaction_type)
-        sheet.write(index, 1, entry.date)
-        sheet.write(index, 2, entry.cat1)
-        sheet.write(index, 3, entry.cat2)
-        sheet.write(index, 4, entry.account1)
-        sheet.write(index, 5, entry.account2)
-        sheet.write(index, 6, entry.amount)
-        sheet.write(index, 7, entry.member)
-        sheet.write(index, 8, entry.merchant)
-        sheet.write(index, 9, entry.project)
-        sheet.write(index, 10, entry.detail)
+            sheet.write(index, 0, transaction_type)
+            sheet.write(index, 1, entry.date)
+            sheet.write(index, 2, entry.cat1)
+            sheet.write(index, 3, entry.cat2)
+            sheet.write(index, 4, entry.account1)  # 收入账户
+            sheet.write(index, 5, entry.amount)
+            sheet.write(index, 6, entry.member)
+            sheet.write(index, 7, entry.merchant)
+            sheet.write(index, 8, entry.project)
+            sheet.write(index, 9, entry.detail)
+
+        else:
+            # 转账工作表：9列（无分类列）
+            sheet = self.sheet_transfer
+            index = self.sheet_entry_index_transfer
+            transaction_type = '转账'
+            self.sheet_entry_index_transfer += 1
+
+            sheet.write(index, 0, transaction_type)
+            sheet.write(index, 1, entry.date)
+            sheet.write(index, 2, entry.account1)  # 转出账户
+            sheet.write(index, 3, entry.account2)  # 转入账户
+            sheet.write(index, 4, entry.amount)
+            sheet.write(index, 5, entry.member)
+            sheet.write(index, 6, entry.merchant)
+            sheet.write(index, 7, entry.project)
+            sheet.write(index, 8, entry.detail)
 
     def save(self, ):
         self.workbook.save(self.file_path)
@@ -85,10 +120,14 @@ if '__main__' == __name__:
     entry = SuiEntry(date='2019-09-14', amount=393.00, detail='财付通-Currify咖喱南京西路')
     xls.add_entry(entry)
 
-    entry = SuiEntry(date='2019-09-14', amount=25, detail='xxx', member='郑之颖')
+    entry = SuiEntry(date='2019-09-14', amount=25.0, detail='xxx', member='郑之颖')
     xls.add_entry(entry)
 
-    entry = SuiEntry(date='2019-09-14', amount=25, detail='yyy', member='郑之颖', classification=1)
+    entry = SuiEntry(date='2019-09-14', amount=25.0, detail='yyy', member='郑之颖', classification=1)
+    xls.add_entry(entry)
+
+    # 测试转账记录
+    entry = SuiEntry(date='2019-09-14', account1='账户A', account2='账户B', amount=100.0, detail='转账测试', classification=2)
     xls.add_entry(entry)
 
     xls.save()
